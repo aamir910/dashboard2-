5b0d57ae6be202293182da327793fd3f96e1904d
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2023-07-04 18:32:11 +0500

    sidebar done

---
+++ Data/Data.jsx
@@ -0,0 +1,37 @@
+
+// import {UilEstate} from '@iconscout/react-unicons'
+
+import {
+    UilEstate,
+    UilClipboardAlt,
+    UilUsersAlt,
+    UilPackage,
+    UilChart,
+    UilSignOutAlt,
+  } from "@iconscout/react-unicons";
+
+  
+export const  SideBarData  = [
+
+    {
+        icon : UilEstate  , 
+        heading : "Dasboard" 
+    }, 
+    {
+        icon : UilClipboardAlt  , 
+        heading : "Order" 
+    }, 
+    {
+        icon : UilUsersAlt  , 
+        heading : "Customer" 
+    }, 
+      {
+        icon : UilChart  , 
+        heading : "Analytics" 
+    }, 
+    {
+        icon : UilPackage  , 
+        heading : "Product " 
+    },
+  
+] ; 
