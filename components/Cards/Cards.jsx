c3fc4f1c321d2d579fc429aea3d474fd5e584cbb
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2023-07-05 05:32:27 +0500

    setup the cards and card and map it

--- components/Cards/Cards.jsx
+++ components/Cards/Cards.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import './Cards.css'
+import Card from '../Card/Card'
+import { cardsData } from '../../Data/Data'
 function Cards() {
   return (
    <div className="cards">
-    card 
-    card
-    card
+         
+         {cardsData.map( ( card , id )=>
+         {
+          return(
+             
+            <div className="parentContainer">
+           <Card 
+             title = {card.title}
+             color = {card.color}
+             barValue = {card.barValue} 
+             value = {card.value}
+             png = {card.png}
+             series = {card.series}
+
+             
+
+           />
+                
+            </div>
+
+
+          )
+            
+         } )}
    </div>
   )
 }
